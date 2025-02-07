080f67a6ee26971eda85ad609b22c4afaec0cd02
Masa Lazic <masalazic56@gmail.com>
2025-02-07 22:48:09 +0100

    Implement instancing

--- engine/include/engine/resources/Mesh.hpp
+++ engine/include/engine/resources/Mesh.hpp
@@ -11,54 +11,53 @@
 #include <engine/resources/Texture.hpp>
 
 namespace engine::resources {
-/**
-* @struct Vertex
-* @brief Represents a vertex in the mesh.
-*/
-struct Vertex {
-    glm::vec3 Position;
-    glm::vec3 Normal;
-    glm::vec2 TexCoords;
+    /**
+    * @struct Vertex
+    * @brief Represents a vertex in the mesh.
+    */
+    struct Vertex {
+        glm::vec3 Position;
+        glm::vec3 Normal;
+        glm::vec2 TexCoords;
 
-    glm::vec3 Tangent;
-    glm::vec3 Bitangent;
-};
+        glm::vec3 Tangent;
+        glm::vec3 Bitangent;
+    };
 
-/**
-* @class Mesh
-* @brief Represents a mesh in the model in the OpenGL context.
-*/
-class Mesh {
-    friend class AssimpSceneProcessor;
-
-public:
+    /**
+    * @class Mesh
+    * @brief Represents a mesh in the model in the OpenGL context.
+    */
+    class Mesh {
+        friend class AssimpSceneProcessor;
+    public:
 
         /**
         * @brief Draws the mesh using a given shader. Called by the @ref Model::draw function to draw all the meshes in the model.
         * @param shader The shader to use for drawing.
         */
         void draw(const Shader *shader);
-        void draw_instanced(const Shader *shader, int amount);
 
         /**
          * @brief Destroys the mesh in the OpenGL context.
          */
         void destroy();
+        uint32_t m_vao{0};
+        uint32_t m_num_indices{0};
+        std::vector<Texture *> m_textures;
+        /**
+        * @brief Constructs a Mesh object.
+        * @param vertices The vertices in the mesh.
+        * @param indices The indices in the mesh.
+        * @param textures The textures in the mesh.
+         */
+        Mesh(const std::vector<Vertex> &vertices, const std::vector<uint32_t> &indices,
+             std::vector<Texture *> textures);
 
-private:
-    /**
-    * @brief Constructs a Mesh object.
-    * @param vertices The vertices in the mesh.
-    * @param indices The indices in the mesh.
-    * @param textures The textures in the mesh.
-     */
-    Mesh(const std::vector<Vertex> &vertices, const std::vector<uint32_t> &indices,
-         std::vector<Texture *> textures);
+    private:
 
-    uint32_t m_vao{0};
-    uint32_t m_num_indices{0};
-    std::vector<Texture *> m_textures;
-};
+
+    };
 } // namespace engine
 
 #endif//MATF_RG_PROJECT_MESH_HPP
